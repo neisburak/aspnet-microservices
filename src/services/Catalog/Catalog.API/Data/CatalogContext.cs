0b50abb7435853cd29bdc4e9ae9092417ea475c3
Burak Neiş <neisburak@hotmail.com>
2022-01-07 01:37:15 +0300

    catalog.api

---
+++ src/services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+using System;
+using Catalog.API.Data.Interfaces;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<String>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<String>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<String>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
