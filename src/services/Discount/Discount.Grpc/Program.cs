857205d08ad8de9d7c5f2fc616db50f8c91138b5
Burak Neiş <neisburak@hotmail.com>
2022-01-09 04:39:40 +0300

    discount.grpc

--- src/services/Discount/Discount.Grpc/Program.cs
+++ src/services/Discount/Discount.Grpc/Program.cs
@@ -1,8 +1,13 @@
-using Discount.API.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Discount.Grpc.Extensions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace Discount.API
+namespace Discount.Grpc
 {
     public class Program
     {
@@ -13,6 +18,8 @@ namespace Discount.API
             host.Run();
         }
 
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
